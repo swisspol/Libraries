aede632b9936c30f89ad2e47e77c27c8c11ba167
Pierre-Olivier Latour <pol@mac.com>
2015-09-22 09:47:02 -0700

    Also test OpenSSL

--- Tests/main.cc
+++ Tests/main.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h>
 
 #include <sqlite3.h>
 #include <webp/encode.h>
@@ -12,8 +13,25 @@
 #include <exiv2/exiv2.hpp>
 #include <libssh2.h>
 #include <curl/curl.h>
+#include <openssl/opensslconf.h>
+#include <openssl/crypto.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
 
 using namespace google::protobuf::io;
+
+static pthread_mutex_t* _openSSLMutexes;
+
+static void _OpenSSLLockingCallback(int mode, int n, const char* file, int line) {
+	if (mode & CRYPTO_LOCK) {
+    pthread_mutex_lock(&_openSSLMutexes[n]);
+	} else {
+    pthread_mutex_unlock(&_openSSLMutexes[n]);
+	}
+}
 
 int main(int argc, const char* argv[]) {
   // Test libsqlite3
@@ -53,6 +71,24 @@ int main(int argc, const char* argv[]) {
   
   // Test libcurl
   curl_version_info(CURLVERSION_NOW);
+  
+  // Test libcrypto / libssl
+#ifndef OPENSSL_THREADS
+#error OpenSSL built without threads
+#endif
+  int count = CRYPTO_num_locks();
+  _openSSLMutexes = (pthread_mutex_t*)calloc(count, sizeof(pthread_mutex_t));
+  for (int i = 0; i < count; ++i) {
+    if (pthread_mutex_init(&_openSSLMutexes[i], NULL)) {
+      abort();
+    }
+  }
+  CRYPTO_set_locking_callback(_OpenSSLLockingCallback);
+  ERR_load_crypto_strings();
+  ERR_load_BIO_strings();
+  SSL_load_error_strings();
+  OpenSSL_add_all_algorithms();
+  OPENSSL_config(NULL);
   
   // We're done!
   printf("OK\n");
