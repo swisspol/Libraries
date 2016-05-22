cdb8255069c0d479d9e2d78f1b7f6807224e9ee8
Pierre-Olivier Latour <pol@mac.com>
2016-05-22 08:01:39 -0700

    Fixes

--- libexif/rebuild.sh
+++ libexif/rebuild.sh
@@ -6,17 +6,17 @@ VERSION="0.6.21"
 source "../rebuild-functions.sh"
 
 # Download source
-if [ ! -f "libexif-${VERSION}.tar.gz" ]
+if [ ! -f "libexif-$VERSION.tar.gz" ]
 then
   curl -O "http://iweb.dl.sourceforge.net/project/libexif/libexif/$VERSION/libexif-$VERSION.tar.gz"
 fi
 
 # Extract source
-rm -rf "libexif-${VERSION}"
-tar -xvf "libexif-${VERSION}.tar.gz"
+rm -rf "libexif-$VERSION"
+tar -xvf "libexif-$VERSION.tar.gz"
 
 # Build library
-pushd "libexif-${VERSION}"
+pushd "libexif-$VERSION"
 
 build_library_macosx
 build_library_iphonesimulator
@@ -25,4 +25,4 @@ build_library_iphoneos
 popd
 
 # Clean up
-rm -rf "libexif-${VERSION}"
+rm -rf "libexif-$VERSION"
--- libpng/rebuild.sh
+++ libpng/rebuild.sh
@@ -6,17 +6,17 @@ VERSION="1.6.18"
 source "../rebuild-functions.sh"
 
 # Download source
-if [ ! -f "libpng-${VERSION}.tar.gz" ]
+if [ ! -f "libpng-$VERSION.tar.gz" ]
 then
-  curl -O "http://iweb.dl.sourceforge.net/project/libpng/libpng16/${VERSION}/libpng-${VERSION}.tar.gz"
+  curl -O "http://iweb.dl.sourceforge.net/project/libpng/libpng16/$VERSION/libpng-$VERSION.tar.gz"
 fi
 
 # Extract source
-rm -rf "libpng-${VERSION}"
-tar -xvf "libpng-${VERSION}.tar.gz"
+rm -rf "libpng-$VERSION"
+tar -xvf "libpng-$VERSION.tar.gz"
 
 # Build library
-pushd "libpng-${VERSION}"
+pushd "libpng-$VERSION"
 
 build_library_macosx
 build_library_iphonesimulator
@@ -25,4 +25,4 @@ build_library_iphoneos
 popd
 
 # Clean up
-rm -rf "libpng-${VERSION}"
+rm -rf "libpng-$VERSION"
